fb88dde6c9b3ce2eee9b1cc5f0172979139dc44d
Duy Pham <duy.phamthe91@gmail.com>
2016-02-02 17:05:25 -0800

    Added First.php
    Signed-off-by: Duy Pham <duy.phamthe91@gmail.com>

---
+++ application/controllers/First.php
@@ -0,0 +1,25 @@
+<?php
+
+class First extends Application 
+{
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    function gimme($id)
+    {
+        $this->data['pagebody'] = 'justone'; 
+
+        $source = $this->quotes->last();
+       
+        $this->data['mug'] = $source['mug'];
+
+        $this->data['who'] = $source['who'];
+
+        $this->data['what'] = $source['what'];
+
+        $this->render();
+    }
+
+}
